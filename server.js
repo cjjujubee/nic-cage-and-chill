e07c04ea3383b4f0e4ff8fe6d049d8d7c882fc47
cjjujubee <connie.jew@gmail.com>
2016-07-12 15:35:08 -0700

    Adds server.js file for database uses

---
+++ server.js
@@ -0,0 +1,26 @@
+require('./db/connect');
+var express = require('express');
+var bodyParser = require('body-parser');
+var userMovieRoutes = require('./routes/userMovie');
+var app = express();
+var passport = require('passport');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(express.static('public'));
+
+app.use('/', userMovieRoutes);
+
+app.listen(8080, function() {
+    console.log('Running on port 8080');
+});
+
+exports.app = app;
