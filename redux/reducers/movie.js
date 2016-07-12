1fc7da8dd0aac9bc1199e2acac49359ada25cf60
cjjujubee <connie.jew@gmail.com>
2016-07-11 22:40:10 -0700

    Adds ability to retrieve random Nicolas Cage movie from API

--- redux/reducers/movie.js
+++ redux/reducers/movie.js
@@ -13,8 +13,30 @@ exports.movieReducer = function(state, action) {
   console.log('state: ', state);
   state = state || initialState;
   if (action.type === actions.GET_MOVIE_SUCCESS) {
-    var movie = action.data
-    console.log('movie', movie);
-    //TODO: figure out how to randomize the movies!!!!!
+    var movie = action.data;
+
+    //finds length of Nicolas Cage movies array to randomly generate
+    //number for random movie
+    var numberOfMovies = movie.length;
+
+    var randomNumber = Math.floor((Math.random() * numberOfMovies) + 1);
+    var randomMovie = movie[randomNumber];
+
+    var newState = update(state, {0: {
+      $set: {
+        show_id: randomMovie.show_id,
+        title: randomMovie.show_title,
+        release: randomMovie.release_year,
+        rating: randomMovie.rating,
+        summary: randomMovie.summary,
+        poster: randomMovie.poster
+      }
+    }});
+    console.log('new state:', newState);
+    state = newState;
   }
+  else if (action.type === actions.GET_MOVIE_ERROR) {
+    console.log(action.err.response.status); //logs out error
+  }
+  return state;
 };
