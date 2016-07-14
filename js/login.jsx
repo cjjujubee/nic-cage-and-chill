6867a452796b7a9d18106f831df775a5f8e836eb
Elias Mason <eliasjmason@gmail.com>
2016-07-14 13:27:24 -0700

    Adds simple login component.

---
+++ js/login.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var connect = require('react-redux').connect;
+var router = require('react-router');
+var Router = router.Router;
+var Route = router.Route;
+var hashHistory = router.hashHistory;
+var IndexRoute = router.IndexRoute;
+var Link = require('react-router').Link;
+
+var Provider = require('react-redux').Provider;
+var store = require('../redux/store');
+
+
+var Login = React.createClass({
+    render: function() {
+        return (
+            <section className="userModal">
+                <h1>Login here</h1>
+            </section>
+        )
+    }
+});
+
+var mapStateToProps = function(state, props) {
+  return {
+    nicCage: state
+  };
+};
+
+var Container = connect(mapStateToProps)(Login);
+
+module.exports = Container;
